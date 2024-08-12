78933cfd10da2fd1ddfbd44933d091374cc0358e
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-13 00:49:53 +1000

    feat: styled hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,18 @@
+import HeroIcon from "../assets/hero-icon.png";
+
 const Hero = () => {
   return (
-    <section>
-      <h1>I'm hero</h1>
+    <section className="hero">
+      <img
+        src={HeroIcon}
+        alt="a grid of various images"
+        className="hero-image"
+      />
+      <h1 className="hero-header">Online Experiences</h1>
+      <p className="hero-text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
     </section>
   );
 };
