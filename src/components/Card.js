78a7d3ed0c22a8acfab38e366670c9ea5882397c
Michael Erzan Andoh <erzanthedev@gmail.com>
2024-08-15 07:23:06 +1000

    fix: improved badgeText logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,14 @@
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "Online";
+  }
+
   return (
     <div className="card">
-      {props.openSpots === 0 && <div className="card-badge">SOLD OUT</div>}
+      {badgeText && <div className="card-badge">{badgeText}</div>}
       <img
         className="card-image"
         src={`/assets/${props.img}`}
